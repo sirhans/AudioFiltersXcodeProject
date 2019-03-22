a5f78be5d9b810836acf36382ac030ab875382dc
Nguyen Minh Tien <tiennm642@gmail.com>
2019-03-22 15:09:18 +0700

    first commit

---
+++ AudioFiltersXcodeProject/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  AudioFiltersXcodeProject
+//
+//  Created by Nguyen Minh Tien on 3/22/19.
+//  Copyright © 2019 BlueMangoo. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
